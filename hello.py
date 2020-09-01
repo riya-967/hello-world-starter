13e80434258b8112c7e70ddb7675bc2d5cf39552
akshatadsule <akshat.adsule@gmail.com>
2020-08-31 20:31:14 -0700

    add hello_test.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-# Print the string "Hello, World!" (make sure to match it exactly!)
+# return the string "Hello, World!" (make sure to match it exactly!)
 
-if __name__ == "__main__":
-  # write code here
+def hello_word():
+	#make sure to return "Hello, World!" here
